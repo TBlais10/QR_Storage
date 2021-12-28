7026ae33c1c0982a638caca358ed1ebd9a0dc18c
TBlais10 <blaislighting@gmail.com>
2021-12-28 15:08:28 -0500

    Initial Commit

---
+++ src/main/java/com/qr_storeage/QR_StoragePt2/QrStoragePt2Application.java
@@ -0,0 +1,13 @@
+package com.qr_storeage.QR_StoragePt2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrStoragePt2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrStoragePt2Application.class, args);
+	}
+
+}
