725cad1217775f9a4d87dccbc62c8f281c1f5f77
TBlais10 <blaislighting@gmail.com>
2021-12-28 17:40:56 -0500

    Recreated model packages and connected mySQL server.

--- src/main/java/com/qr_storeage/QR_StoragePt2/QrStoragePt2Application.java
+++ src/main/java/com/qr_storeage/QR_StoragePt2/QrStoragePt2Application.java
@@ -2,6 +2,9 @@ package com.qr_storeage.QR_StoragePt2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class QrStoragePt2Application {
@@ -9,5 +12,11 @@ public class QrStoragePt2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(QrStoragePt2Application.class, args);
 	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder){
+		return builder.build();
+	}
+
 
 }
