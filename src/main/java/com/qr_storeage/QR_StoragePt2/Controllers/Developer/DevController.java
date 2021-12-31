1f02a72bda9095ca9348170f6db8c1fd4a25dfe5
TBlais10 <blaislighting@gmail.com>
2021-12-31 15:47:41 -0500

    Created Post, Put, and Delete methods in DevController and added to Developer Repository.

--- src/main/java/com/qr_storeage/QR_StoragePt2/Controllers/Developer/DevController.java
+++ src/main/java/com/qr_storeage/QR_StoragePt2/Controllers/Developer/DevController.java
@@ -5,7 +5,9 @@ import com.qr_storeage.QR_StoragePt2.Repositories.AvatarRepository;
 import com.qr_storeage.QR_StoragePt2.Repositories.DeveloperRepository;
 import com.qr_storeage.QR_StoragePt2.Repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -36,9 +38,32 @@ public class DevController {
         return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
+    @GetMapping("/facility/{facility}")
+    public ResponseEntity<List<Developer>> getDeveloperByFacility(@PathVariable String facility){
+        return new ResponseEntity<>(repository.findByFacility(facility, Sort.by("name")), HttpStatus.OK);
+    }
+
     @PostMapping
-    public @ResponseBody Developer createDeveloper(@RequestBody Developer newDeveloper){
-        return repository.save(newDeveloper);
+    public ResponseEntity<Developer> createDeveloper(@RequestBody Developer newDeveloper){
+        return new ResponseEntity<>(repository.save(newDeveloper), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public @ResponseBody Developer updateDeveloper(@PathVariable Long id, @RequestBody Developer updates){
+
+        Developer developer = repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        if (updates.getName() != null) developer.setName(updates.getName());
+        if (updates.getEmail() != null) developer.setEmail(updates.getEmail());
+        if (updates.getFacility() != null) developer.setFacility(updates.getFacility());
+
+        return repository.save(developer);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> destroyDeveloper(@PathVariable Long id){
+        repository.deleteById(id);
+        return new ResponseEntity<>("Deleted user " + id, HttpStatus.OK);
     }
 
 }
--- src/main/java/com/qr_storeage/QR_StoragePt2/Repositories/DeveloperRepository.java
+++ src/main/java/com/qr_storeage/QR_StoragePt2/Repositories/DeveloperRepository.java
@@ -1,9 +1,14 @@
 package com.qr_storeage.QR_StoragePt2.Repositories;
 
 import com.qr_storeage.QR_StoragePt2.Models.Developers.Developer;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface DeveloperRepository extends JpaRepository<Developer, Long> {
 
+    //TODO: Research more methods that work w/ sorting and displaying info
+    List<Developer> findByFacility(String facility, Sort sort);
 
 }
