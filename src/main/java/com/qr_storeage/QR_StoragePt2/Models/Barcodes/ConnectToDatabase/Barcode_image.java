e2846a287a4de3f970e04f18e303243d37c21869
TBlais10 <blaislighting@gmail.com>
2022-01-03 22:12:46 -0500

    Added more dependancies for creating barcodes, created barcode package and classes for creating pdf and image versions of barcodes.

---
+++ src/main/java/com/qr_storeage/QR_StoragePt2/Models/Barcodes/ConnectToDatabase/Barcode_image.java
@@ -0,0 +1,43 @@
+package com.qr_storeage.QR_StoragePt2.Models.Barcodes.ConnectToDatabase;
+
+import org.krysalis.barcode4j.impl.code128.Code128Bean;
+import org.krysalis.barcode4j.output.bitmap.BitmapCanvasProvider;
+import org.krysalis.barcode4j.tools.UnitConv;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Barcode_image {
+
+    private String ImgFolderPath = "C:\\Users\\kille\\Documents\\careerdevs\\2021_12\\QR_StoragePt2\\QRCodes\\Images";
+
+    public void createBarCode128(String fileName){
+        try {
+            Code128Bean bean = new Code128Bean();
+            final int dpi = 160;
+
+            bean.setModuleWidth((UnitConv.in2mm(2.8f / dpi)));
+
+            bean.doQuietZone(false);
+
+            File outputFile = new File(ImgFolderPath + fileName + ".JPG");
+
+            FileOutputStream out = new FileOutputStream(outputFile);
+
+            BitmapCanvasProvider canvas = new BitmapCanvasProvider(
+                    out, "image/x-png", dpi, BufferedImage.TYPE_BYTE_BINARY, false,0
+            );
+
+            bean.generateBarcode(canvas, fileName);
+
+            canvas.finish();
+
+            System.out.println("Barcode for " + fileName + " successfully created.");
+        } catch (IOException ex){
+            ex.printStackTrace();
+        }
+    }
+
+}
