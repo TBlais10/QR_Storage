07d67b073aa8a56a8cc597cedd7920ad5dbd1816
TBlais10 <blaislighting@gmail.com>
2022-03-18 23:53:00 -0400

    Continued working on authentication

---
+++ src/main/java/com/qr_storeage/QR_StoragePt2/Models/Bodies/JwtResponse.java
@@ -0,0 +1,14 @@
+package com.qr_storeage.QR_StoragePt2.Models.Bodies;
+
+public class JwtResponse {
+
+    private final String jwttoken;
+
+    public JwtResponse(String jwttoken) {
+        this.jwttoken = jwttoken;
+    }
+
+    public String getJwttoken() {
+        return jwttoken;
+    }
+}
