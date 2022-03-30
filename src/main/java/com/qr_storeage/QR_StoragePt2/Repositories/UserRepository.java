7219aed13623f265d81d321cc8097b2900937b93
TBlais10 <blaislighting@gmail.com>
2022-03-29 22:27:47 -0400

    Continuing to develop authentication

--- src/main/java/com/qr_storeage/QR_StoragePt2/Repositories/UserRepository.java
+++ src/main/java/com/qr_storeage/QR_StoragePt2/Repositories/UserRepository.java
@@ -3,5 +3,10 @@ package com.qr_storeage.QR_StoragePt2.Repositories;
 import com.qr_storeage.QR_StoragePt2.Models.Auth.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
 }
