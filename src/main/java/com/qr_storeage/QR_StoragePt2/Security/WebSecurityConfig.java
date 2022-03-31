c6b8c40c8850fdf603fdaff7042acc70765ff720
TBlais10 <blaislighting@gmail.com>
2022-03-30 23:32:14 -0400

    Continuing to develop Authentication

--- src/main/java/com/qr_storeage/QR_StoragePt2/Security/WebSecurityConfig.java
+++ src/main/java/com/qr_storeage/QR_StoragePt2/Security/WebSecurityConfig.java
@@ -1,13 +1,33 @@
 package com.qr_storeage.QR_StoragePt2.Security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.AuthenticationEntryPoint;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(
         prePostEnabled = true
 )
-public class WebSecurityConfig {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    UserDetailService userDetailService;
+
+    @Autowired
+    private AuthenticationEntryPoint unauthorizedHandler;
+
+    @Bean
+    public AuthTokenFilter authenticationJwtTokenFilter(){
+        return new AuthTokenFilter();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder) throws Exception{
+        authenticationManagerBuilder.userDetailService(userDetailService);
+    }
 }
