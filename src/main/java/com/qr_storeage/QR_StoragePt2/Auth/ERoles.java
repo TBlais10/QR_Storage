4072ec6ae344e5eddcd980225308de45efeeaaea
TBlais10 <blaislighting@gmail.com>
2022-04-18 17:08:34 -0400

    Completed Auth Package

---
+++ src/main/java/com/qr_storeage/QR_StoragePt2/Auth/ERoles.java
@@ -0,0 +1,7 @@
+package com.qr_storeage.QR_StoragePt2.Auth;
+
+public enum ERoles {
+    ROLE_ADMIN,
+    ROLE_MODERATOR,
+    ROLE_USER
+}
---
+++ src/main/java/com/qr_storeage/QR_StoragePt2/Auth/Role.java
@@ -0,0 +1,40 @@
+package com.qr_storeage.QR_StoragePt2.Auth;
+
+import com.qr_storeage.QR_StoragePt2.Models.Auth.ERole;
+
+import javax.persistence.*;
+
+@Entity
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+
+    public Role() {
+    }
+
+    public Role(Long id, ERole name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public ERole getName() {
+        return name;
+    }
+
+    public void setName(ERole name) {
+        this.name = name;
+    }
+}
