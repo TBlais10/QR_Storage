49a5d4280d46f8e41df7a1281eafdea4fd953043
TBlais10 <blaislighting@gmail.com>
2022-04-18 16:59:25 -0400

    Completed Payload Package

---
+++ src/main/java/com/qr_storeage/QR_StoragePt2/Payload/Response/MessageResponse.java
@@ -0,0 +1,13 @@
+package com.qr_storeage.QR_StoragePt2.Payload.Response;
+
+public class MessageResponse {
+    private String message;
+
+    public MessageResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
